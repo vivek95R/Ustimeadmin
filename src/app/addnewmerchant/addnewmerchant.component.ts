460c786f7ce13d67e1d79682330521529b3019ad
vivek95R <vivekraghuraman95@gmail.com>
2019-05-19 12:05:29 +0530

    addnewregistration

--- src/app/addnewmerchant/addnewmerchant.component.ts
+++ src/app/addnewmerchant/addnewmerchant.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,ViewChild, ViewEncapsulation } from '@angular/core';
 import * as $ from 'jquery';
 @Component({
   selector: 'app-addnewmerchant',
   templateUrl: './addnewmerchant.component.html',
+  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./addnewmerchant.component.scss']
 })
 export class AddnewmerchantComponent implements OnInit {
-
+  @ViewChild('ngbTabset') t: any;
+  thirdtab:boolean = true;
+  enable(){
+    this.thirdtab =false;
+    this.t.select('tab-selectbyid2')
+  }
   constructor() { }
 
   ngOnInit() {
